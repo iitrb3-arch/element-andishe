b343fbbeaeb28ab7b0fa76efac867be95913515f
iitrb3-arch <iitrb3@gmail.com>
2025-10-04 13:51:48 -0700

    Branding: Andishe-chat, default HS https://edu97.ir; skip onboarding→login; hide signup; add school tagline; single APK CI

--- vector/src/main/java/im/vector/app/features/onboarding/OnboardingActivity.kt
+++ vector/src/main/java/im/vector/app/features/onboarding/OnboardingActivity.kt
@@ -12,23 +12,13 @@ import android.content.Intent
 import android.net.Uri
 import android.view.View
 import dagger.hilt.android.AndroidEntryPoint
-import im.vector.app.core.extensions.lazyViewModel
-import im.vector.app.core.extensions.validateBackPressed
 import im.vector.app.core.platform.VectorBaseActivity
-import im.vector.app.core.platform.lifecycleAwareLazy
 import im.vector.app.databinding.ActivityLoginBinding
 import im.vector.app.features.login.LoginConfig
 import im.vector.app.features.pin.UnlockedActivity
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class OnboardingActivity : VectorBaseActivity<ActivityLoginBinding>(), UnlockedActivity {
-
-    private val onboardingVariant by lifecycleAwareLazy {
-        onboardingVariantFactory.create(this, views = views, onboardingViewModel = lazyViewModel())
-    }
-
-    @Inject lateinit var onboardingVariantFactory: OnboardingVariantFactory
 
     override fun getBinding() = ActivityLoginBinding.inflate(layoutInflater)
 
@@ -37,25 +27,9 @@ class OnboardingActivity : VectorBaseActivity<ActivityLoginBinding>(), UnlockedA
     override val rootView: View
         get() = views.coordinatorLayout
 
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        onboardingVariant.onNewIntent(intent)
-    }
-
-    @Suppress("OVERRIDE_DEPRECATION")
-    override fun onBackPressed() {
-        validateBackPressed {
-            super.onBackPressed()
-        }
-    }
-
     override fun initUiAndData() {
-        onboardingVariant.initUiAndData(isFirstCreation())
-    }
-
-    // Hack for AccountCreatedFragment
-    fun setIsLoading(isLoading: Boolean) {
-        onboardingVariant.setIsLoading(isLoading)
+        navigator.openLogin(this)
+        finish()
     }
 
     companion object {
